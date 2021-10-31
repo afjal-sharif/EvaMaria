dd4c40384ea57507df135e4440bc29927f5b7464
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-10-31 19:09:49 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,7 +21,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Go Inline ➕', switch_inline_query='')
             ],[
             InlineKeyboardButton('🔍 Search Here 🔍', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 BDH 🤖', url='https://t.me/BangladeshHoarding')
@@ -64,7 +64,7 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Go Inline ➕', switch_inline_query='')
             ],[
             InlineKeyboardButton('🔍 Search Here 🔍', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 BDH 🤖', url='https://t.me/BangladeshHoarding')
