2d2f90406930aa4fd092d0c477ea9bd5c8511042
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-11-21 18:36:28 +0600

    Update info.py

--- info.py
+++ info.py
@@ -22,6 +22,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
+CHAT_ID = int(environ.get("CHAT_ID", -1001190259319))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
