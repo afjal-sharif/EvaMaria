7f4c0706e8f5706bd3a732fac56a33095e233889
Afjal Sharif <67854373+afjal-sharif@users.noreply.github.com>
2021-12-01 03:57:25 +0600

    Update info.py

--- info.py
+++ info.py
@@ -34,8 +34,8 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'BangladeshHoarding')
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
 
 #ForceSub MOD
-CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", "@BangladeshHoarding")
-WARN_MESSAGE = os.environ.get("WARN_MESSAGE", "ভাই, দুঃখিত, আপনাকে সাময়িক মিউট করা হয়েছে, নিচের চ্যানেল গুলোতে জয়েন করার পর আনমিউট বাটনে ক্লিক করে আনমিউট হয়ে নিন, আনমিউট হওয়ার পর গ্রুপে ব্যবহার করতে পারবেন।")
+CHANNEL_USERNAME = environ.get("CHANNEL_USERNAME", "@BangladeshHoarding")
+WARN_MESSAGE = environ.get("WARN_MESSAGE", "ভাই, দুঃখিত, আপনাকে সাময়িক মিউট করা হয়েছে, নিচের চ্যানেল গুলোতে জয়েন করার পর আনমিউট বাটনে ক্লিক করে আনমিউট হয়ে নিন, আনমিউট হওয়ার পর গ্রুপে ব্যবহার করতে পারবেন।")
 
 # GDrive Search Config
 RESULTS_COUNT = 6  # NOTE Nuber of results to show, 4 is better
